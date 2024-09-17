26f0a2322b3de3e7184464c8da84d0442e5e38ec
Saurav Jha <sauravjha5299@gmail.com>
2024-09-17 19:04:14 +0530

    Added table and database class

--- InMemoryDB/src/Database.java
+++ InMemoryDB/src/Database.java
@@ -1,3 +1,56 @@
-public class Database {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Database {
+    private Map<String, Table> tables;
+     
+    public Database(){
+        this.tables = new HashMap<>();
+    }
+
+    public void createTable(String tableName){
+        if(!(tables.containsKey(tableName))){
+            tables.put(tableName, new Table(tableName));
+        }
+    }
+
+    public void dropTable(String tableName){
+        tables.remove(tableName);
+    }
+
+    public Table getTable(String tableName){
+        return tables.get(tableName);
+    }
+
+    public void insertIntoTable(String tableName, Row row) throws Exception {
+        Table table= tables.get(tableName);
+        if(table != null){
+            table.insertRow(row);
+        }
+    }
+
+    public void deleteFromTable(String tableName, Object primaryKeyValue){
+        Table table = tables.get(tableName);
+        if(table != null){
+            table.deleteRow(primaryKeyValue);
+        }
+    }
+
+    public void updateTable(String tableName, Object primaryKeyValue, String columnName, Object newValue){
+        Table table = tables.get(tableName);
+        if(table != null){
+            Column column = table.getColumnByName(columnName);
+            if(column != null){
+                table.updateRow(primaryKeyValue, column, newValue);
+            }
+        }
+    }
+
+    public void selectAllFromTable(String tableName){
+        Table table= tables.get(tableName);
+        if(table != null){
+            System.out.println(table.selectAll());
+        }
+    }
+    
 }
--- InMemoryDB/src/Table.java
+++ InMemoryDB/src/Table.java
@@ -1,3 +1,66 @@
+import java.util.*;
 public class Table {
+    private String name;
+    private List<Column> columns;
+    private List<Row> rows;
+    private Column primaryKeyCol;
+    private Set<Object> primaryKeys;
+
+    public Table(String name){
+        this.name = name;
+        this.columns = new ArrayList<>();
+        this.rows = new ArrayList<>();
+        this.primaryKeys = new HashSet<>();
+    }
+
+    public void addColumn(String name, DataType type, int length, boolean isPrimaryKey){
+        Column column = new Column(name, type, length, isPrimaryKey);
+        columns.add(column);
+        if(isPrimaryKey){
+            primaryKeyCol = column;
+        }
+    }
+
+    public void insertRow(Row row) throws Exception{
+        if(primaryKeyCol != null){
+            Object primaryKeyValue = row.getColumnValue(primaryKeyCol);
+            if(primaryKeys.contains(primaryKeyValue)){
+                throw new Exception("Primary key constraints voilated");
+            }
+            primaryKeys.add(primaryKeyValue);
+        }
+        rows.add(row);
+    }
+
+    public void deleteRow(Object primaryKeyValue){
+        rows.removeIf(row -> row.getColumnValue(primaryKeyCol).equals(primaryKeyValue));
+        primaryKeys.remove(primaryKeyValue);
+    }
+
+    public List<Row> selectAll(){
+        return rows;
+    }
+
+    public void updateRow(Object primaryKeyValue, Column column, Object newValue){
+        for(Row row: rows){
+            if(row.getColumnValue(primaryKeyCol).equals(primaryKeyValue)){
+                row.setColumnValue(column, newValue);
+            }
+        }
+    }
+
+    public Column getColumnByName(String columnName){
+        for(Column col: columns){
+            if(col.getName().equals(columnName)){
+                return col;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        return "Table: " + name + "\nColumns: " + columns + "\nRows: " + rows;
+    }
 
 }
